f9db6e76a94f2f50ad61f25e301e1424f73bfec3
An Jorge <an.jorge@icloud.com>
2022-04-17 23:51:57 +0100

    Exercicio 12 up

---
+++ main/src/desafios/iniciante/12-calculandoDescontos.kt
@@ -0,0 +1,18 @@
+package desafios.iniciante
+
+/*
+12 — Calculando Descontos
+Exercício 012: faça um algoritmo que leia o preço de um produto e mostre o seu novo preço, com 5% de desconto.
+
+*/
+
+fun main() {
+    val article = 100
+    val discount = 50
+    val sum = article * discount / 100
+    println(
+        """
+        The article cost $article kz with $discount off
+        Value of discount is $sum kz.
+        """)
+}
