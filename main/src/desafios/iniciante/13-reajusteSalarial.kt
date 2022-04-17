6ab43af6a89a36a8aea3706e4eb1d834bcffd4cf
An Jorge <an.jorge@icloud.com>
2022-04-17 23:57:49 +0100

    Exercicio 13 up

---
+++ main/src/desafios/iniciante/13-reajusteSalarial.kt
@@ -0,0 +1,14 @@
+package desafios.iniciante
+
+/*
+13 — Reajuste Salarial
+Exercício 013: Faça um algoritmo que leia o salário de um funcionário e
+mostre o seu novo salário, com 15% de aumento
+ */
+
+
+
+fun main(salary: Float, increase: Float) : String {
+    return "Salario actual: $salary, com aumento de $increase = ${salary + (salary * increase / 100)}"
+
+}
