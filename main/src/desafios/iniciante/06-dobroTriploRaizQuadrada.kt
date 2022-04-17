a0ff97b2b95e26f0a2631a9934ca745cc8683e0c
An Jorge <an.jorge@icloud.com>
2022-04-17 23:03:28 +0100

    Exercicio 06 up

---
+++ main/src/desafios/iniciante/06-dobroTriploRaizQuadrada.kt
@@ -0,0 +1,18 @@
+package desafios.iniciante
+
+import kotlin.math.sqrt
+
+/*
+
+06 — Dobro, Triplo, Raiz Quadrada
+Exercício 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+ */
+
+fun main() {
+    println("DIGITE O NÚMERO: ")
+    val myNumb = readLine()!!.toInt()
+    println("Dobro de $myNumb -> ${myNumb * 2}")
+    println("Triplo de $myNumb -> ${myNumb * 3}")
+    println("Raiz quadrada de $myNumb -> ${sqrt(myNumb.toFloat())}")
+}
